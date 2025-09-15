f73a09140e4647ae358f67ba4e8ba13257477a9c
RyanWahinya <ryan.wahinya@student.moringaschool.com>
2025-09-15 03:41:14 +0300

    new commit

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -16,12 +15,37 @@ db.init_app(app)
 
 api = Api(app)
 
+# ----------- Resource Definitions -----------
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return [plant.to_dict() for plant in plants], 200
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return new_plant.to_dict(), 201
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = db.session.get(Plant, id)
+        if plant:
+            return plant.to_dict(), 200
+        return {"error": "Plant not found"}, 404
+
+# ----------- Add Routes to API -----------
+
+api.add_resource(Plants, "/plants")
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+# ----------- Run App -----------
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
